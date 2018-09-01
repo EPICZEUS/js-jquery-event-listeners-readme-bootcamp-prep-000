24db6df60b7edb95b0f9d86d4463e3e011fdb4d0
Sam B <s77broz@aol.com>
2018-09-01 04:23:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,7 @@
-//define functions here
+function getIt() {
+  $("p").on("click", () => alert("Hey!"));
+}
 
 $(document).ready(function(){
-  
-  function getIt() {
-    $("p").on("click", () => alert("Hey!"));
-  }
-  
+  getIt();
 });
