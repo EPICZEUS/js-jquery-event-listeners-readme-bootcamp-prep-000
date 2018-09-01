76a37662bf82cbaaf7f1049858bf2ea8874d5cd8
Sam B <s77broz@aol.com>
2018-09-01 04:25:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,13 @@
 function getIt() {
-  $("p").on("click", () => alert("Hey!"));
+  $("p").on("click", () => {
+    alert("Hey!")
+  });
 }
 
 function frameIt() {
   $("img").on("load", () => {
     $("img").addClass("tasty");
-  })
+  });
 }
 
 $(document).ready(function(){
